e9cf9ba117a9c59b3945352edd04fe6b9a3bb453
JahnaviSananse <jahnavisananse@gmail.com>
2021-09-03 23:57:46 +0530

    test cases resolved

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,9 @@
 class Player
   attr_accessor :credits, :active
 
-  def sub_credit(sub_creds)
+  def sub_credits(sub_creds)
+    raise StandardError if sub_creds == 0
+
     @credits -= sub_creds
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ describe Player do
   it 'calculates the correct credits remaining' do
     player = Player.new
     player.credits = 1.5
-    player.sub_credit(1.3)
+    player.sub_credits(1.3)
     expect(player.credits).to be_within(0.0001).of 0.19999
   end
 
@@ -12,5 +12,11 @@ describe Player do
     player = Player.new
     player.active = 'YES'
     expect(player.active).to be_truthy
+  end
+
+  it 'returns an error when sub_credits is passes a zero credit value' do
+    player = Player.new
+    player.credits = 2
+    expect { player.sub_credits(0) }.to raise_exception(StandardError)
   end
 end
